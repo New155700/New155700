6470dc6a425d7f0856404f88e28db74f0fe2fa64
New155700 <120209183+New155700@users.noreply.github.com>
2026-05-13 02:43:13 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,22 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-
   callbacks: {
-    async session({ session }) {
+    async session({ session }: any) {
       if (session?.user) {
-
-        // OWNER EMAIL
-        if (session.user.email === 'newgod155700@gmail.com') {
-          session.user.role = 'OWNER';
-        } else {
-          session.user.role = 'USER';
-        }
+        // เปลี่ยนเป็นเมลคุณเพื่อเป็นเจ้าของร้าน
+        session.user.role = session.user.email === "newgod155700@gmail.com" ? "OWNER" : "USER";
       }
-
       return session;
     },
   },
-
-  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
