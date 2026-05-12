56f35497f0127c09360561af6f34c908203b636b
New155700 <120209183+New155700@users.noreply.github.com>
2026-05-13 04:03:15 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import './globals.css';
+import './globals.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'NNSHOP | Premium Store',
   description: 'ระบบร้านค้าอันดับ 1',
-};
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="th">
       <body className="bg-[#050505] text-white">
         {children}
       </body>
     </html>
-  );
+  )
 }
